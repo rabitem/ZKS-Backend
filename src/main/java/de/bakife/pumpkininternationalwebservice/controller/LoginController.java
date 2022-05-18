b40daad398570509dd5d242f7dc13bd929c58f65
batuhan.oezkul@web.de <batuhan.oezkul@web.de>
2022-05-18 08:51:10 +0200

    Added the is loggedin endpoint

--- src/main/java/de/bakife/pumpkininternationalwebservice/controller/LoginController.java
+++ src/main/java/de/bakife/pumpkininternationalwebservice/controller/LoginController.java
@@ -1,19 +1,19 @@
 package de.bakife.pumpkininternationalwebservice.controller;
 
+import de.bakife.pumpkininternationalwebservice.entities.User;
 import de.bakife.pumpkininternationalwebservice.services.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 /**
  * @author rabitem
+ * @author senseiB2
  */
 @Controller
 @Slf4j
@@ -47,4 +47,20 @@ public class LoginController {
         Map<String, String> result = this.loginService.login(rfid, macAddress);
         return new ResponseEntity<>(result.get("message"), HttpStatus.valueOf(Integer.parseInt(result.get("status"))));
     }
+
+    /**
+     * Checks whether user is logged in or not
+     * @param payload The payload containing the rfid.
+     * @return True if logged in; false if logged out.
+     */
+    @GetMapping(value = "/isLoggedIn", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<Boolean> isLoggedIn(@RequestBody Map<String, Object> payload){
+        String rfid = (String) payload.get("rfid");
+        log.info("user is logged in request with rfid: {}", rfid);
+        User user = this.loginService.checkUserByRfid(rfid);
+        return new ResponseEntity<>(user.getLocation().getId() != 1, HttpStatus.OK);
+    }
+
+
 }
