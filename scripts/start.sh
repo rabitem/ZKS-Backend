28a7a050429f65e5b9cd4a723456619d9d1efa75
rabitem <f.huisinga@arcor.de>
2022-05-18 11:28:37 +0200

    Clean before starting

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 echo "Made with love by r̸a̷b̸i̸t̸e̶m̶"
 echo "Starting up..."
-screen -dmS tomcat mvn spring-boot:run
+screen -dmS tomcat mvn clean spring-boot:run
 echo "Application will be started in background."
 exit 0
